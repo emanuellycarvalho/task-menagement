9169dc4ec5de5177d8eba59b8451f3807e44a69b
Emanuelly <emanuellyvcarv@gmail.com>
2023-12-12 23:39:40 -0300

    wip: organization database settings

---
+++ backend/database/factories/OrganizationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ModelsOrganization>
+ */
+class OrganizationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company,
+            'category' => $this->faker->word,
+            'email' => $this->faker->unique()->safeEmail,
+            'address' => $this->faker->streetAddress,
+            'city' => $this->faker->city,
+            'country' => $this->faker->country,
+        ];
+    }
+}
