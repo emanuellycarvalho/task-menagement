584e40313d32c0c579047e2b76258d15616282da
Emanuelly <emanuellyvcarv@gmail.com>
2023-12-14 10:21:30 -0300

    wip: add user routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrganizationController;
 
 /*
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('organizations', OrganizationController::class);
+Route::apiResource('users', UserController::class);
