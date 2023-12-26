303713609019c9ee7b8e4b7d0dc3f07d511df1ba
Emanuelly <emanuellyvcarv@gmail.com>
2023-12-26 12:55:33 -0300

    wip: importing sanctum and setting cors

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
 const instance = axios.create({
   baseURL: 'http://localhost:8000/api',
 });
